4a16bc3f16014bf175118f804e00d6bee0f5e79c
WallysLS <wallys.souza@gmail.com>
2022-05-22 11:10:08 -0300

    created first components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Button/>
       </header>
     </div>
   );
